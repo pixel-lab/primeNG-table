786f0e2961069ff87a2a3fa64e8cc90d240c4c33
Sankha <pixellabdesk@gmail.com>
2021-02-10 13:37:02 +0530

    search-edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,27 +23,30 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class AppComponent {
     products: Product[];
+    stored: Product[];
     product: Product;
     users: Array<User> = [];
     userDynamic: any = {};
     submitted: boolean;
     productDialog: boolean;
     setLang = [
-        { name: 'Arabic', code: 'ar' }           
+        { name: 'Arabic', code: 'ar' }   
     ];
+
     selectedLanguage: Language;
     constructor(private productService: ProductService) {
 
     }
     ngOnInit() {
-        this.productService.getProductsWithOrdersSmall().then(data =>this.products = data);
+        this.productService.getProductsWithOrdersSmall().then(data =>{
+            this.products = data;
+            this.stored = data;
+        });
         this.userDynamic = { firistname: "", lastname: "", language: this.setLang, selectedLanguage:this.setLang[0]  };
         this.users.push(this.userDynamic);       
     }
-
    
     addRow(rowNum: number) {
-        console.log(rowNum);
         this.userDynamic = { firistname: "", lastname: "", language: this.setLang, selectedLanguage:this.setLang[0]  };
         this.users.push(this.userDynamic);        
         console.log(this.users);
@@ -56,14 +59,14 @@ export class AppComponent {
             return true;
         }
     }
-    editMainTable(p:Product){
-        debugger;
+    editMainTable(p:Product){        
         this.productService.editProduct(p.id).then(data => {           
             //this.product = { ... this.product, id:data[0].data };
             this.product = {};
             this.product= {
                 firstname: data[0].firstName,
-                lastname: data[0].lastName
+                lastname: data[0].lastName,
+                id: p.id
             }             
             this.users = [{ 
                 firstname: data[0].localized[0].firstName,
@@ -83,34 +86,49 @@ export class AppComponent {
         this.submitted = false;
     }
     saveProduct() {
+        debugger;
        this.submitted = true;
        this.product = {
+        "id": this.product.id,
         "profilePicture": "https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/Movies/2012_NewFallTitles/TheDictator_8926700-35693-001._V390128733_SX1080_.jpg",
         "placeOfBirth": "Iraq",
         "localized": {
           "en": {
             "firstName": this.product.firstname,
             "lastName": this.product.lastname,
-            "fullName": this.product.lastname +this.product.lastname
+            "fullName": this.product.firstname +  ' '+this.product.lastname
           },
           "ar": {
             "firstName": this.users[0].firstname,
             "lastName": this.users[0].lastname,
-            "fullName": this.users[0].firstname + this.users[0].lastname
+            "fullName": this.users[0].firstname + ' '+  this.users[0].lastname
           }
         }
       }
         
-        this.productService.addProduct(this.product).then(data => {           
-            //this.product = { ... this.product, id:data[0].data };
-            this.product = { ... this.product, id:Math.random().toString() };
-            this.products.push(this.product);
-            this.products = this.dataManupulate([...this.products]);
-            this.productDialog = false;
+        this.productService.addProduct(this.product).then(data => {   
+            if(!this.product.id){      
+                //this.product = { ... this.product, id:data[0].data };
+                this.product = { ... this.product, id:Math.random().toString() };
+                this.products.push(this.product);
+                this.products = this.dataManupulate([...this.products]); 
+            }  
+            else {
+                let manupulateArr = [];
+                this.products = this.products.map(i => {
+                    
+                    if(i.id === this.product.id){
+                        debugger;
+                        manupulateArr.push(this.product);
+                        i = this.dataManupulate(manupulateArr)[0];
+                    }
+                    return i;
+                } )
+            }
             this.userDynamic = { firistname: "", lastname: "", language: this.setLang, selectedLanguage:this.setLang[0]  };
             this.users=[this.userDynamic];
             this.product = {};
-            console.log(this.products);
+            this.productDialog = false;
         })
         
     }
@@ -126,7 +144,14 @@ export class AppComponent {
         this.submitted = false;
         this.productDialog = true;
     }
-   
+    filterGlobal(value:string){
+        this.products = this.stored;
+        let filter = this.products.filter(i => {
+            debugger;
+           return i.fullName.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1
+        }); 
+        this.products = filter;
+    }
    dataManupulate(data){
        debugger;
         let create = data.map(i => {
